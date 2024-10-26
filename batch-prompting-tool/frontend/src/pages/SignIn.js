6a309c3d071be9a3a4b6256584f4b3413f7138e9
nicolevowell5 <nicolevowell5@gmail.com>
2024-10-27 01:47:40 +0800

    This is second update: SignIn, SinUp, Table UI updating

--- batch-prompting-tool/frontend/src/pages/SignIn.js
+++ batch-prompting-tool/frontend/src/pages/SignIn.js
@@ -4,11 +4,14 @@ import SigninComponent from "../component/SigninComponent";
 const Signin = () => {
   return (
     <React.Fragment>
+      <div className="row d-flex justify-content-center w-100">
+
       <h2 className="text-center pt-4 pb-4">SignIn</h2>
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <SigninComponent />
         </div>
+      </div>
       </div>
     </React.Fragment>
   );
--- batch-prompting-tool/frontend/src/pages/SignUp.js
+++ batch-prompting-tool/frontend/src/pages/SignUp.js
@@ -3,11 +3,11 @@ import SignupComponent from "../component/SignUpComponet";
 const SignUp = () => {
   return (
     <React.Fragment>
+      <div className="row d-flex justify-content-center w-100">
       <h2 className="text-center pt-4 pb-4">SignUp</h2>
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
+      <div className="row d-flex justify-content-center w-100">
           <SignupComponent />
-        </div>
+      </div>
       </div>
     </React.Fragment>
   );
